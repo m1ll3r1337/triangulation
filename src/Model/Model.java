774d71fdff1d0dc4975a0fcd2f25aefa082ba076
m1ll3r1337 <mah235715@gmail.com>
2023-11-15 11:54:52 +0300

    Cloned obj reader

---
+++ src/Model/Model.java
@@ -0,0 +1,16 @@
+package Model;
+
+import com.cgvsu.math.Vector2f;
+import com.cgvsu.math.Vector3f;
+import math.Vector2f;
+import math.Vector3f;
+
+import java.util.*;
+
+public class Model {
+
+    public ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
+    public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
+    public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
+    public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+}
